29045c5c1f93f97477d5635b008252e6d8ee1c31
Logan Willis <loganw0315@gmail.com>
2021-12-14 14:07:04 -0600

    added handleDelete to BlogList component

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -6,13 +6,24 @@ const Home = () => {
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-      ]);
+    ]);
+
+    
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => id !== blog.id)
+        setBlogs(newBlogs)
+    }  
+
+    useEffect(() => {
+        console.log("use effect ran");
+    })
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs"/>
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
         </div>
-      );
+    );
 }
  
 export default Home;
