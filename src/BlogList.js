34f118f67c0a1905e763de1b42ce36a0095a436a
Logan Willis <loganw0315@gmail.com>
2021-12-14 13:49:57 -0600

    changed props in BlogList to be destructured

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,8 +1,8 @@
-const BlogList = (props) => {
-
+const BlogList = ({blogs, title}) => {
     return ( 
         <div className="blog-list">
-            {props.blogs.map((blog) => (
+            <h2>{title}</h2>
+            {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
