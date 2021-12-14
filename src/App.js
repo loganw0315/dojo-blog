fcd2de2402d54047e14c27a52fcb03ec000719e5
Logan Willis <loganw0315@gmail.com>
2021-12-14 15:34:32 -0600

    added BlogDetails component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Navbar from './Navbar';
 import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 
 function App() {
@@ -16,6 +17,9 @@ function App() {
             </Route>
             <Route exact path="/create">  
               <Create />
+            </Route>
+            <Route exact path="/blogs/:id">  
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
